8f231e9e65ccdf6a494411142fd0cdc3b9528e5b
DiegoNY <nalvartediego40@gmail.com>
2023-05-26 08:50:57 -0500

    Error de bulto y nmeor de items corregido

--- app/crear_estructura/CrearEstructuraGuiaRemision.ts
+++ app/crear_estructura/CrearEstructuraGuiaRemision.ts
@@ -6,35 +6,30 @@ export const CrearEstructuraGuiaRemision = (
   items: DetalleItems[]
 ): GuiaRemision[] => {
   const GuiaRemision: GuiaRemision[] = [];
-
-  items.map((item) => {
-    if (item.SERNUMGUIA == documento.SERNUMGUIA) {
-      GuiaRemision.push({
-        CodMotTras: documento.CODMOTTRAS,
-        DesMotTras: documento.DESMOTTRAS,
-        CodModTras: documento.MODTRASGUI,
-        DesModTras: ValidarTipoTraslado(documento.MODTRASGUI),
-        FechaIniTraslado: documento.FECINITRAS,
-        PesoBruto: documento.PSOBRUTOTB == 0 ? 1 : Number(documento.PSOBRUTOTB),
-        NroBultos: Number(documento.NUMTOTBULT),
-        TipoDocConductor:
-          documento.MODTRASGUI == "01" ? Number(documento.TIPDOCTRAN) : 1,
-        NroDocConductor:
-          documento.MODTRASGUI == "02"
-            ? documento.NOMDOCCHOF
-            : documento.NUMDOCTRAN,
-        DireccionLlegada: documento.DIRLLEGADA,
-        UbigeoLlegada: Number(documento.UBILLEGADA),
-        DireccionPartida: documento.DIRPARTIDA,
-        UbigeoPartida: Number(documento.UBIPARTIDA),
-        NombreConductor:
-          documento.MODTRASGUI == "01"
-            ? documento.NOMTRANSPO
-            : documento.NOMBCHOFER,
-        NroLicencia: documento.NUMLICCHOF,
-        Placa: documento.NUMPLACA.replace("-", ""),
-      });
-    }
+  GuiaRemision.push({
+    CodMotTras: documento.CODMOTTRAS,
+    DesMotTras: documento.DESMOTTRAS,
+    CodModTras: documento.MODTRASGUI,
+    DesModTras: ValidarTipoTraslado(documento.MODTRASGUI),
+    FechaIniTraslado: documento.FECINITRAS,
+    PesoBruto: documento.PSOBRUTOTB == 0 ? 1 : Number(documento.PSOBRUTOTB),
+    NroBultos: Number(documento.NUMTOTBULT),
+    TipoDocConductor:
+      documento.MODTRASGUI == "01" ? Number(documento.TIPDOCTRAN) : 1,
+    NroDocConductor:
+      documento.MODTRASGUI == "02"
+        ? documento.NOMDOCCHOF
+        : documento.NUMDOCTRAN,
+    DireccionLlegada: documento.DIRLLEGADA,
+    UbigeoLlegada: Number(documento.UBILLEGADA),
+    DireccionPartida: documento.DIRPARTIDA,
+    UbigeoPartida: Number(documento.UBIPARTIDA),
+    NombreConductor:
+      documento.MODTRASGUI == "01"
+        ? documento.NOMTRANSPO
+        : documento.NOMBCHOFER,
+    NroLicencia: documento.NUMLICCHOF,
+    Placa: documento.NUMPLACA.replace("-", ""),
   });
 
   return GuiaRemision;
